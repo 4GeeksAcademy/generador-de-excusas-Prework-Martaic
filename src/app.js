27dc5785862c20ffd83263fabdb9f6e003c81ca9
Marta <159387209+martaic@users.noreply.github.com>
2025-01-16 16:43:08 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateExcuse() {
+    let subjects = ["A raccoon", "A pirate", "A wizard", "Superman", "A ghost"];
+    let verbs = ["stole", "buried", "teleported", "broke", "lost"];
+    let objects = [
+      "keys",
+      "my money",
+      "my homeworks",
+      "my dron",
+      "my computer"
+    ];
+    let places = [
+      "in the garage",
+      "in the backyard",
+      "in the kitchen",
+      "on the street",
+      "at the park"
+    ];
+
+    let subject = subjects[Math.floor(Math.random() * subjects.length)];
+    let verb = verbs[Math.floor(Math.random() * verbs.length)];
+    let object = objects[Math.floor(Math.random() * objects.length)];
+    let place = places[Math.floor(Math.random() * places.length)];
+
+    let excuse = `${subject} ${verb} ${object} ${place}.`;
+
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+
+  document
+    .getElementById("generator")
+    .addEventListener("click", generateExcuse);
 };
